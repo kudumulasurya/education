139c70ed41fa6a24e0ea2aa2d7ac393eb7d1a5d5
kudumulasurya <121920898+kudumulasurya@users.noreply.github.com>
2023-01-08 19:22:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let navbar = document.querySelector('.header .navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+};
+
+let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
+
+registerBtn.onclick = () =>{
+  registerBtn.classList.add('active');
+  loginBtn.classList.remove('active');
+  document.querySelector('.account-form .login-form').classList.remove('active');
+  document.querySelector('.account-form .register-form').classList.add('active');
+};
+
+loginBtn.onclick = () =>{
+  registerBtn.classList.remove('active');
+  loginBtn.classList.add('active');
+  document.querySelector('.account-form .login-form').classList.add('active');
+  document.querySelector('.account-form .register-form').classList.remove('active');
+};
+
+let accountForm = document.querySelector('.account-form')
+
+document.querySelector('#account-btn').onclick = () =>{
+  accountForm.classList.add('active');
+}
+
+document.querySelector('#close-form').onclick = () =>{
+  accountForm.classList.remove('active');
+};
+
+$(document).ready(function(){
+  //jquery for toggle sub menus
+  $('.sub-btn').click(function(){
+    $(this).next('.sub-menu').slideToggle();
+    $(this).find('.dropdown').toggleClass('rotate');
+  });
+
+  //jquery for expand and collapse the sidebar
+  $('.menu-btn1').click(function(){
+    $('.side-bar').addClass('active');
+    $('.menu-btn1').css("visibility", "hidden");
+  });
+
+  $('.close-btn').click(function(){
+    $('.side-bar').removeClass('active');
+    $('.menu-btn1').css("visibility", "visible");
+  });
+});
